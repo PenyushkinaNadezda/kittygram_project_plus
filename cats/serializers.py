b08db6c37c3067629434ffc4b342e07a82341b2d
Penyushkina Nadezda <najia00@qq.com>
2026-04-20 18:37:34 +0800

    kittygram

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,17 +1,46 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
 
 from .models import Achievement, AchievementCat, Cat, Owner
 
 
+class CatListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
+
+
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = Hex2NameColor()
 
     class Meta:
         model = Cat
@@ -21,8 +50,12 @@ class CatSerializer(serializers.ModelSerializer):
             'color',
             'birth_year',
             'owner',
-            'achievements'
+            'achievements',
+            'age'
         )
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
